1de3a066134e3dc9608cb1374ef7ca19e539132a
syam <105439154+syam-64@users.noreply.github.com>
2022-08-06 15:08:16 +0530

    Delete PasswordGenerator.py

--- PasswordGenerator.py
+++
@@ -1,9 +0,0 @@
-import random
-numbers="0123456789"
-lower="abcdefghijklmnopqrstuvwxyz"
-upper="ABCDEFGHIJKLMOPQRSTUVWXYZ"
-special="~!@#$%^&*()<>,"
-password=numbers+lower+upper+special
-n=int(input("Enter the legth of password:"))
-print("Generating Password.......")
-print("Your password is >>>>>>>>>> "+"".join(random.sample(password,n)))
